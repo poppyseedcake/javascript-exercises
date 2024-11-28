8d3d8b1fdbc073b6c2b019343825ca63d6b5e460
WojtekM <1062789+poppyseedcake@users.noreply.github.com>
2024-11-28 22:28:21 +0100

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeated = '';
 
+    if (num < 0) return "ERROR";
+    
+    for (let i = 0; i < num; i++){
+        repeated += string;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
