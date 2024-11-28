ca0597e7e5431ebfa7fe59a9c1ade8740efe5a2c
WojtekM <1062789+poppyseedcake@users.noreply.github.com>
2024-11-28 22:47:48 +0100

    03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splited = string.split("");
+    let reversed = "";
 
+    while (splited.length > 0) {
+        reversed += splited.pop();
+    }
+    return reversed;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
