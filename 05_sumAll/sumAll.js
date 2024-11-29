4ddc7e900337bbd803d2b296d63a9db2131db6ac
WojtekM <1062789+poppyseedcake@users.noreply.github.com>
2024-11-30 00:24:32 +0100

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 ) return "ERROR";
+    if (!Number.isInteger(a) || !Number.isInteger(b) ) return "ERROR";
+
+    // if (!(a instanceof Number) || !(b instanceof Number)) return "ERROR";
+    
+    let sum = 0;
+
+
+    if (a > b) {
+        let tempA = a;
+        a = b;
+        b = tempA;
+    }
+
+    for (a; a <= b; a++) {
+        sum += a;
+    }
+    return sum;
 
 };
-
 // Do not edit below this line
 module.exports = sumAll;
