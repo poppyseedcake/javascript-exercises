1f632428e91cc4cc5d4e192952eb349934545d4e
WojtekM <1062789+poppyseedcake@users.noreply.github.com>
2024-11-29 23:45:49 +0100

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    for (let i = 1; i < arguments.length; i++) {
+        let position;
+        do {
+            position = arr.indexOf(arguments[i]);
+            if (position != -1) arr.splice(position, 1);
+        } while (position != -1);
+    }
+    console.log(arr);
+    return arr;
 };
-
+removeFromArray([2, 3, 4, 4], 2, 4);
 // Do not edit below this line
 module.exports = removeFromArray;
