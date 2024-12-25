aac299b62bd2dc169fd9162836e81b792be4eb16
WojtekM <1062789+poppyseedcake@users.noreply.github.com>
2024-12-26 00:08:46 +0100

    sa

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return parseInt(a) + parseInt(b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return parseInt(a) - parseInt(b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, item) => {
+    return sum += item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, item) => {
+    return sum *= item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = 1;
+  for (let i = 1; i <= b; i++) {
+    // console.log(`i = $[i]`);
+    // console.log(arr[0], arr[1]);
+    result *= a;
+    // console.log({result});
+  }
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorial = 1;
+  
+  for (let i = 1; i <= number; i++) {
+    factorial *= i;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
