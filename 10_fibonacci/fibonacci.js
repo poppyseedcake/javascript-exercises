32644d88fb9466db21923e14cf4c9f4c79aa11e2
WojtekM <1062789+poppyseedcake@users.noreply.github.com>
2024-12-26 12:26:43 +0100

    10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
+    let prev = 1;
+    let curr = 1;
+    if (num <= 0) return 0;
 
+    for (let i = 3; i <= num; i++) {
+        [prev, curr] = [curr, prev + curr];        
+    }
+    return curr;
 };
 
 // Do not edit below this line
