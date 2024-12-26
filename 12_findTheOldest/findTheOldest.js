072de5e6a4d54c02e517b9870e6c4d2d04b36e94
WojtekM <1062789+poppyseedcake@users.noreply.github.com>
2024-12-26 23:39:14 +0100

    12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b) => {
+        if (!("yearOfDeath" in a)) a.yearOfDeath = (new Date()).getFullYear();
+        if (!("yearOfDeath" in b)) b.yearOfDeath = (new Date()).getFullYear();
 
+        const lastguy = a.yearOfDeath - a.yearOfBirth;
+        const nextguy = b.yearOfDeath - b.yearOfBirth;
+        return lastguy > nextguy ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
