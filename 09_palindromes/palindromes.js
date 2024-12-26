e86c6e9d64d4bc488fa2cff64f6293774b795398
WojtekM <1062789+poppyseedcake@users.noreply.github.com>
2024-12-26 01:02:38 +0100

    dadSA

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    const arrStr = str.split("");
 
+    for (let i = arrStr.length - 1; i >= 0; i--) {
+        if (arrStr[i] === " " || arrStr[i] === "," || arrStr[i] === "." || arrStr[i] === "!" || arrStr[i] === "?") {
+            arrStr.splice(i, 1);
+        };
+    }
+
+    const arrStrReversed = arrStr.slice().reverse();
+    return (arrStr.join("") === arrStrReversed.join("")) ? true : false;
 };
 
 // Do not edit below this line
