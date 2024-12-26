66789d4208727e739e5d2c17848ca0ef74f1dc33
WojtekM <1062789+poppyseedcake@users.noreply.github.com>
2024-12-26 12:52:49 +0100

    11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const result = [];
+    books.forEach(element => {
+        result.push(element.title);
+    });
+    return result;
 };
 
 // Do not edit below this line
